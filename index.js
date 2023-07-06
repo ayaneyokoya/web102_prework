cbab13f6a37e63bc621398c0aa13aa81f7b93ea3
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-06 15:04:53 -0400

    select & display top 2 games; challenge 7

--- index.js
+++ index.js
@@ -169,7 +169,12 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [first, second, ...others] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGame = document.createElement('p').innerHTML = `${first.name}`;
+firstGameContainer.append(topGame);
 
 // do the same for the runner up item
+const runnerUp = document.createElement('p').innerHTML = `${second.name}`;
+secondGameContainer.append(runnerUp);
